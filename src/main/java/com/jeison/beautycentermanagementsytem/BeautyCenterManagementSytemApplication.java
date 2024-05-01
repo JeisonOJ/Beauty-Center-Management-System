e26985c791834bf966f378ecca7abb6007134828
Jeison <jeisonortiz1516@gmail.com>
2024-05-01 11:53:31 -0500

    Beauty center project

---
+++ src/main/java/com/jeison/beautycentermanagementsytem/BeautyCenterManagementSytemApplication.java
@@ -0,0 +1,13 @@
+package com.jeison.beautycentermanagementsytem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyCenterManagementSytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyCenterManagementSytemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeison/beautycentermanagementsytem/BeautyCenterManagementSytemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeison.beautycentermanagementsytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyCenterManagementSytemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
